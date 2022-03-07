4558c733d794ba4f35b72ebeb7b2f658fa5b7326
Limerancy <limerancy@gmail.com>
2022-03-07 16:46:32 +0800

    done without test

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,12 @@
 
 using namespace std;
 
-sjtu::priority_queue<int> q;
+sjtu::priority_queue<int> q, p;
 
 int main() {
     for (int i = 0; i < 10; ++i)
         q.push(i);
-    for (int i = 0; i < 10; ++i) {
-        int v = q.top();
-        q.pop();
-        cout << v << endl;
-    }
+    for (int i = 10; i < 20; ++i)
+        p.push(i);
+    p.merge(q);
 }
